eb407e505989a58df96d9e1179e2811f6d1f04a8
Julia <julianunezc@gmail.com>
2025-03-11 13:38:29 +0100

    Add Waveform class in a single file

---
+++ spectrum_generator/waveform.py
@@ -0,0 +1,49 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+
+class Waveform:
+    """A class to represent a waveform."""
+
+    def __init__(
+        self,
+        time: np.ndarray,
+        amp: np.ndarray,
+        srate: float = 5120,
+    ):
+        """Initializes a Waveform object with time and amplitude arrays.
+
+        time (np.ndarray): A numpy array containing the time values of the waveform.
+        amp (np.ndarray): A numpy array containing the amplitude values of the waveform.
+        srate (float): The sampling rate of the waveform, default is 5120 Hz.
+
+        """
+        self.time = time
+        self.amp = amp
+        self.srate = srate
+
+    @classmethod
+    def from_csv(cls, filename: str):
+        """Loads waveform data from a CSV file."""
+        current_dir = os.path.dirname(__file__)
+        filepath = os.path.join(current_dir, "..", "data", filename)
+        data = pd.read_csv(filepath)
+        time = data["t"].to_numpy()
+        amp = data["amp"].to_numpy()
+        return cls(time, amp)
+
+    def __repr__(self) -> str:
+        """Visualization of the waveform.
+
+        Returns:
+        str: A string representation of the Waveform instance.
+
+        """
+        return f"Waveform(srate={self.srate}, duration={self.time[-1]}s)"
+
+
+# Prueba cargando los datos
+waveform = Waveform.from_csv("wave_data.csv")
+print(waveform.__repr__())
