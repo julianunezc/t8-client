c409a36d8fd0cf1c5fe79cf515821050b6a6e0f4
Julia <julianunezc@gmail.com>
2025-03-12 12:35:18 +0100

    Add Hanning window method to Waveform class

--- spectrum_generator/waveform.py
+++ spectrum_generator/waveform.py
@@ -18,11 +18,14 @@ class Waveform:
         time (np.ndarray): A numpy array containing the time values of the waveform.
         amp (np.ndarray): A numpy array containing the amplitude values of the waveform.
         srate (float): The sampling rate of the waveform, default is 5120 Hz.
+        windowed_amps (np.ndarray): A numpy array containing
+                                    the windowed amplitude values.
 
         """
         self.time = time
         self.amp = amp
         self.srate = srate
+        self.windowed_amps = None
 
     @classmethod
     def from_csv(cls, filename: str):
@@ -34,6 +37,18 @@ class Waveform:
         amp = data["amp"].to_numpy()
         return cls(time, amp)
 
+    def hanning_window(self) -> np.ndarray:
+        """Applies a Hanning window to the waveform.
+        
+        Returns:
+        np.ndarray: A numpy array containing the windowed amplitude values.
+
+        """
+        num_samples = len(self.amp)
+        window = np.hanning(num_samples)
+        self.windowed_amps = self.amp * window
+        return self.windowed_amps
+
     def __repr__(self) -> str:
         """Visualization of the waveform.
 
@@ -42,8 +57,3 @@ class Waveform:
 
         """
         return f"Waveform(srate={self.srate}, duration={self.time[-1]}s)"
-
-
-# Prueba cargando los datos
-waveform = Waveform.from_csv("wave_data.csv")
-print(waveform.__repr__())
