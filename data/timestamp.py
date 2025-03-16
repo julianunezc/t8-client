3d4d6baa7912bcee918ead2a29c911a65e3d1918
Julia <julianunezc@gmail.com>
2025-03-16 22:19:58 +0100

    Remove unused scripts and refactor Spectrum and Waveform classes to utilize common data fetching utilities

--- data/timestamp.py
+++
@@ -1,9 +0,0 @@
-"""This script converts a UTC date and time to a Unix timestamp."""
-
-from datetime import datetime, timezone
-
-utc_time = datetime(2019, 4, 11, 18, 25, 54, tzinfo=timezone.utc)
-
-unix_timestamp = utc_time.timestamp()
-
-print(f"Unix timestamp: {unix_timestamp}")
--- spectra_comparator/spectrum.py
+++ spectra_comparator/spectrum.py
@@ -1,38 +1,79 @@
 import os
 
 import numpy as np
-import pandas as pd
+from common import fetch_data, get_unix_timestamp, zint_to_float
+from dotenv import load_dotenv
 
 
 class Spectrum:
     """A class to represent a spectrum."""
 
     def __init__(self, freq: np.ndarray, amp: np.ndarray):
-        """Initializes a Waveform object with time and amplitude arrays.
+        """Initializes a Spectrum object with time and amplitude arrays.
 
         Parameters:
         time (np.ndarray): Contains the frequency values (in Hz).
         amp (np.ndarray): Contains the amplitude values corresponding to the freqs.
-
         """
         self.freq = freq
         self.amp = amp
 
     @classmethod
-    def from_csv(cls, filename: str):
-        """Loads spectrum data from a CSV file."""
-        current_dir = os.path.dirname(__file__)
-        filepath = os.path.join(current_dir, "..", "data", filename)
-        data = pd.read_csv(filepath)
-        freq = data["freq"].to_numpy()
-        amp = data["amp"].to_numpy()
-        return cls(freq, amp)
+    def from_api(
+        cls, year: int, month: int, day: int, hour: int, minute: int, second: int
+    ):
+        """Loads spectrum data from API using parameters stored in environment variables
+        and returns a Spectrum object.
+
+        Parameters:
+        year (int): The year.
+        month (int): The month.
+        day (int): The day.
+        hour (int): The hour.
+        minute (int): The minute.
+        second (int): The second.
+
+        Returns:
+        Spectrum: A Spectrum object with the data loaded from the API.
+        """
+        # Get configuration values from .env
+        load_dotenv()
+        USER = os.getenv("T8_USER")
+        PASS = os.getenv("T8_PASSWORD")
+        DEVICE_IP = os.getenv("DEVICE_IP")
+        MACHINE = os.getenv("MACHINE")
+        POINT = os.getenv("POINT")
+        PMODE = os.getenv("PMODE")
+
+        # Calculate Unix timestamp using the provided date and time
+        timestamp = get_unix_timestamp(year, month, day, hour, minute, second)
+
+        # API URL
+        url = f"http://{DEVICE_IP}/rest/spectra/{MACHINE}/{POINT}/{PMODE}/{timestamp}"
+
+        # Fetch the spectrum data from the API
+        r = fetch_data(url, USER, PASS)
+
+        # Process the spectrum data
+        fmin = r.get("min_freq", 0)
+        fmax = r["max_freq"]
+        factor = r["factor"]
+        raw = r["data"]
+
+        # Decode and convert the spectrum data
+        sp = zint_to_float(raw)
+        sp *= factor
+
+        # Create freq array
+        freq = np.linspace(fmin, fmax, len(sp))
+
+        return cls(freq, sp)
 
     def __repr__(self) -> str:
         """Visualization of the spectrum.
 
         Returns:
-        str: A string representation of the Waveform instance.
+        str: A string representation of the Spectrum instance.
 
         """
         return (
