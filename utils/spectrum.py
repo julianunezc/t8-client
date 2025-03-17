ac2c0678d23eb796221fab9a6b1614585a3eeda4
Julia <julianunezc@gmail.com>
2025-03-17 13:10:17 +0100

    Add Spectrum class to handle spectrum data and filtering from API

--- utils/spectrum.py
+++ utils/spectrum.py
@@ -1,58 +1,45 @@
-import os
-
 import numpy as np
-from common import fetch_data, get_unix_timestamp, zint_to_float
-from dotenv import load_dotenv
+
+import utils.functions as fun
 
 
 class Spectrum:
     """A class to represent a spectrum."""
 
     def __init__(self, freq: np.ndarray, amp: np.ndarray):
-        """Initializes a Spectrum object with time and amplitude arrays.
+        """Initializes a Spectrum object with frequency and amplitude arrays.
 
         Parameters:
-        time (np.ndarray): Contains the frequency values (in Hz).
+        freq (np.ndarray): Contains the frequency values (in Hz).
         amp (np.ndarray): Contains the amplitude values corresponding to the freqs.
+        filtered_freq (np.ndarray): Contains the filtered frequency values (in Hz).
+        filtered_amp (np.ndarray): Contains the filtered amplitude values
+                                    corresponding to the freqs.
         """
         self.freq = freq
         self.amp = amp
+        self.filtered_freq = None
+        self.filtered_amp = None
 
     @classmethod
-    def from_api(
-        cls, year: int, month: int, day: int, hour: int, minute: int, second: int
-    ):
+    def from_api(cls):
         """Loads spectrum data from API using parameters stored in environment variables
         and returns a Spectrum object.
-
-        Parameters:
-        year (int): The year.
-        month (int): The month.
-        day (int): The day.
-        hour (int): The hour.
-        minute (int): The minute.
-        second (int): The second.
 
         Returns:
         Spectrum: A Spectrum object with the data loaded from the API.
         """
         # Get configuration values from .env
-        load_dotenv()
-        USER = os.getenv("T8_USER")
-        PASS = os.getenv("T8_PASSWORD")
-        DEVICE_IP = os.getenv("DEVICE_IP")
-        MACHINE = os.getenv("MACHINE")
-        POINT = os.getenv("POINT")
-        PMODE = os.getenv("PMODE")
+        USER, PASS, DEVICE_IP, MACHINE, POINT, PMODE, TIME = fun.load_env_variables()
 
         # Calculate Unix timestamp using the provided date and time
-        timestamp = get_unix_timestamp(year, month, day, hour, minute, second)
+        timestamp = fun.get_unix_timestamp(TIME)
 
         # API URL
         url = f"http://{DEVICE_IP}/rest/spectra/{MACHINE}/{POINT}/{PMODE}/{timestamp}"
 
         # Fetch the spectrum data from the API
-        r = fetch_data(url, USER, PASS)
+        r = fun.fetch_data(url, USER, PASS)
 
         # Process the spectrum data
         fmin = r.get("min_freq", 0)
@@ -61,13 +48,26 @@ class Spectrum:
         raw = r["data"]
 
         # Decode and convert the spectrum data
-        sp = zint_to_float(raw)
+        sp = fun.zint_to_float(raw)
         sp *= factor
 
         # Create freq array
         freq = np.linspace(fmin, fmax, len(sp))
-
         return cls(freq, sp)
+
+    def apply_filter(self, fmin: float, fmax: float):
+        """Filters the frequencies and amplitudes within a specified range.
+
+        Parameters:
+        fmin (float): The minimum frequency for filtering.
+        fmax (float): The maximum frequency for filtering.
+
+        Returns:
+        Updates the filtered_freq and filtered_amp attributes.
+        """
+        filter_mask = (self.freq >= fmin) & (self.freq <= fmax)
+        self.filtered_freq = self.freq[filter_mask]
+        self.filtered_amp = self.amp[filter_mask]
 
     def __repr__(self) -> str:
         """Visualization of the spectrum.
